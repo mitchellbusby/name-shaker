f7267975138778b1b8fdebc24183e733ab81fa39
Mitchell Busby <mitchbusby@gmail.com>
2016-08-14 17:40:46 +1000

    Moves to use 1990 census data

--- pyscripts/bucket_names.py
+++ pyscripts/bucket_names.py
@@ -5,14 +5,15 @@ Shouts 2 github/matthewja for writing this
 
 import collections, csv, json
 
-NAMES_PATH = 'yob2015.txt'
+NAMES_PATH = 'census1990.csv'
 OUTPUT_PATH = 'output.json'
 
 name_to_freq = collections.defaultdict(int)
 total = 0
 with open(NAMES_PATH) as names_file:
-    for name, _, freq in csv.reader(names_file):
-        freq = int(freq)
+    for name, freq in csv.reader(names_file):
+        #Normalise frequency out a bit
+        freq = int(float(freq) * 1000)
         name_to_freq[name] += freq  # += accomodates for unisex names.
         total += freq
 
@@ -36,5 +37,11 @@ output_model = {
     'vcommon': common
 }
 
+def sample_output_model(output_model):
+    for field in output_model:
+        print (field, output_model[field][:20])
+
 with open(OUTPUT_PATH, 'w+') as output_file:
     output_file.write(json.dumps(output_model))
+
+sample_output_model(output_model)
